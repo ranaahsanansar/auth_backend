dc18bc87c394d0ccf141cfb04a5ccc751eb2d4b3
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-07-03 01:21:43 -0700

    Create DataBase Connection

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-import dotenv from "doten";
+import dotenv from "dotenv";
 dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import connectDb from './config/connectdb.js';
 
+
+const app = express();
+const port = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// cors policy 
+app.use(cors()); 
+
+// DataBase Connection 
+connectDb(DATABASE_URL);
+
+// JSON API use 
+app.use(express.json());
+
+
+
+
+app.listen(port , ()=>{
+    console.log('Server listening');
+});
